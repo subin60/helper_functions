c7bc631a7df2a816f69594ffff73c3b9b4d82409
Subin Jose Thomas <subin.thomas@web.de>
2023-05-16 23:58:10 +0200

    added docstring and comments

--- plot_history.py
+++ plot_history.py
@@ -1,28 +1,40 @@
 def plot_history(history):
-  import matplotlib.pyplot as plt
-  #from importlib import reload
-  #plt=reload(plt)
+  """
+  Plots the training and validation loss and accuracy of a trained model.
+
+  Args:
+  history: History object. Outputs of the fit() function of a keras model.
+
+  Returns:
+  Two plots. One for loss and one for accuracy.
+  """
   
+  # Importing the necessary library for plotting
+  import matplotlib.pyplot as plt
+  
+  # Extracting loss and accuracy for both training and validation from the History object
   loss = history.history['loss']
   accuracy = history.history['accuracy']
-
   val_loss = history.history['val_loss']
   val_accuracy = history.history['val_accuracy']
 
+  # Getting the number of epochs the model was trained for
   epochs = range(len(history.history['loss']))
 
+  # Plotting training and validation loss
   plt.figure()
-  plt.plot(epochs,loss, label='training loss')
-  plt.plot(epochs,val_loss,label='validation loss')
-  plt.title('Loss')
-  plt.xlabel('Epochs')
-  plt.ylabel('Loss')
-  plt.legend()
+  plt.plot(epochs, loss, label='training loss')
+  plt.plot(epochs, val_loss, label='validation loss')
+  plt.title('Loss')  # Title of the plot
+  plt.xlabel('Epochs')  # X-axis label
+  plt.ylabel('Loss')  # Y-axis label
+  plt.legend()  # Legend to differentiate between training and validation loss
 
+  # Plotting training and validation accuracy
   plt.figure()
-  plt.plot(epochs,accuracy,label='training accuracy')
-  plt.plot(epochs,val_accuracy,label='validation accuracy')
-  plt.title('Accuracy')
-  plt.xlabel('Epochs')
-  plt.ylabel('Accuracy')
-  plt.legend()
+  plt.plot(epochs, accuracy, label='training accuracy')
+  plt.plot(epochs, val_accuracy, label='validation accuracy')
+  plt.title('Accuracy')  # Title of the plot
+  plt.xlabel('Epochs')  # X-axis label
+  plt.ylabel('Accuracy')  # Y-axis label
+  plt.legend()  # Legend to differentiate between training and validation accuracy
