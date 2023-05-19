744b231caacdf8a6265b4f95cd57780391b9e327
Subin Jose Thomas <subin.thomas@web.de>
2023-05-19 15:49:02 +0200

    Update helper_functions_tf.py

--- helper_functions_tf.py
+++ helper_functions_tf.py
@@ -271,3 +271,60 @@ def walk_through_dir(dir_path):
         # Print the counts and the current directory path
         print(f"There are {num_dirs} directories and {num_files} images in '{dirpath}'.")
 
+        
+import matplotlib.pyplot as plt
+
+def compare_history(original_history, new_history, initial_epochs=5):
+    """
+    This function compares the training history of two different training runs, 
+    and plots their accuracy and loss for training and validation sets.
+    Useful for visualizing the effects of fine-tuning the model.
+
+    Parameters:
+    original_history (History): History object of the original model. 
+                                Contains the training and validation loss and accuracy for each epoch.
+    new_history (History): History object of the new model (after fine-tuning). 
+                           Contains the training and validation loss and accuracy for each epoch.
+    initial_epochs (int): Number of epochs the original model was trained for, 
+                          helps to align the two plots correctly.
+
+    Returns:
+    None
+    """
+    # Get original history measurements
+    acc = original_history.history["accuracy"]
+    loss = original_history.history["loss"]
+
+    val_acc = original_history.history["val_accuracy"]
+    val_loss = original_history.history["val_loss"]
+
+    # Combine original history with new history
+    total_acc = acc + new_history.history["accuracy"]
+    total_loss = loss + new_history.history["loss"]
+
+    total_val_acc = val_acc + new_history.history["val_accuracy"]
+    total_val_loss = val_loss + new_history.history["val_loss"]
+
+    # Make plots
+    plt.figure(figsize=(8, 8))
+    
+    # Plot training and validation accuracy
+    plt.subplot(2, 1, 1)
+    plt.plot(total_acc, label='Training Accuracy')
+    plt.plot(total_val_acc, label='Validation Accuracy')
+    plt.plot([initial_epochs-1, initial_epochs-1], plt.ylim(), label='Start Fine Tuning') 
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+    
+    # Plot training and validation loss
+    plt.subplot(2, 1, 2)
+    plt.plot(total_loss, label='Training Loss')
+    plt.plot(total_val_loss, label='Validation Loss')
+    plt.plot([initial_epochs-1, initial_epochs-1], plt.ylim(), label='Start Fine Tuning') 
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+    plt.xlabel('epoch')
+    
+    # Show the plot
+    plt.show()        
+
