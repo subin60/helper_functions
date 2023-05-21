12f0aa893d9fd8ba8d5a7d676425662055b1b38f
Subin Jose Thomas <subin.thomas@web.de>
2023-05-21 08:37:37 +0200

    Update helper_functions_tf.py

--- helper_functions_tf.py
+++ helper_functions_tf.py
@@ -497,4 +497,72 @@ def unfreeze_model_layers(model='base_model', num_last_layers_trainable=10):
 
     print(f"The last {num_last_layers_trainable} layers of {model.name} are now trainable.")
   
-    
+  
+  
+import itertools
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics import confusion_matrix
+
+def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_size=15, norm=False, savefig=False, x_rotation=0): 
+  """
+  Makes a labelled confusion matrix comparing predictions and ground truth labels.
+
+  Args:
+    y_true: Array of truth labels (must be same shape as y_pred).
+    y_pred: Array of predicted labels (must be same shape as y_true).
+    classes: Array of class labels (e.g. string form). If `None`, integer labels are used.
+    figsize: Size of output figure (default=(10, 10)).
+    text_size: Size of output figure text (default=15).
+    norm: normalize values or not (default=False).
+    savefig: save confusion matrix to file (default=False).
+    x_rotation: rotation angle for xticks (default=0).
+  
+  Returns:
+    A labelled confusion matrix plot comparing y_true and y_pred.
+  """  
+  cm = confusion_matrix(y_true, y_pred)
+  cm_norm = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis]
+  n_classes = cm.shape[0]
+
+  fig, ax = plt.subplots(figsize=figsize)
+  cax = ax.matshow(cm, cmap=plt.cm.Blues)
+  fig.colorbar(cax)
+
+  if classes:
+    labels = classes
+  else:
+    labels = np.arange(cm.shape[0])
+  
+  ax.set(title="Confusion Matrix",
+         xlabel="Predicted label",
+         ylabel="True label",
+         xticks=np.arange(n_classes),
+         yticks=np.arange(n_classes), 
+         xticklabels=labels, 
+         yticklabels=labels)
+
+  ax.xaxis.set_label_position("bottom")
+  ax.xaxis.tick_bottom()
+
+  threshold = (cm.max() + cm.min()) / 2.
+
+  for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+    if norm:
+      plt.text(j, i, f"{cm[i, j]} ({cm_norm[i, j]*100:.1f}%)",
+              horizontalalignment="center",
+              color="white" if cm[i, j] > threshold else "black",
+              size=text_size)
+    else:
+      plt.text(j, i, f"{cm[i, j]}",
+              horizontalalignment="center",
+              color="white" if cm[i, j] > threshold else "black",
+              size=text_size)
+
+  # Adjusting the rotation of x-axis labels
+  plt.xticks(rotation=x_rotation, fontsize=text_size)
+  plt.yticks(fontsize=text_size)
+  
+  if savefig:
+    fig.savefig("confusion_matrix.png")
+  
